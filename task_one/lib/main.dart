2969a35f09a94f295596d4f06fabd3cbc823a9e8
Sheeraz Ali <sheerazali7676@gmail.com>
2023-05-19 22:49:33 +0500

    initial commit

--- task_one/lib/main.dart
+++ task_one/lib/main.dart
@@ -11,7 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      
       home: HomePage(),
     );
   }
---
+++ task_one/lib/sec_pge.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class SecPage extends StatelessWidget {
+  const SecPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Huuuuu"),
+      ),
+    );
+  }
+}
