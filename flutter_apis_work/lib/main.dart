e7468708021583b47a0fb7801c83885beae01d94
Sheeraz Ali <sheerazali7676@gmail.com>
2023-07-24 18:50:35 +0500

    Done With Get

--- flutter_apis_work/lib/main.dart
+++ flutter_apis_work/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'example_one_using_model_class.dart';
-import 'example_one_without_model_class.dart';
+import 'getexamples/example_one_using_model_class.dart';
+import 'getexamples/example_one_without_model_class.dart';
 
 void main() {
   runApp( const ApiWork());
